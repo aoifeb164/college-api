d6bb3d80975b7dac8854fcc0c5fae051a5a9ae7f
aoife164 <n00182228@student.iadt.ie>
2021-02-21 12:00:21 +0000

    commit

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -1,4 +1,9 @@
 <?php
+# @Date:   2020-12-31T09:12:14+00:00
+# @Last modified time: 2021-02-21T12:00:11+00:00
+
+
+
 
 namespace App\Models;
 
@@ -8,9 +13,10 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     use HasFactory;
-    
+
     public function enrolments()
     {
         return $this->hasMany('App\Models\Enrolment');
     }
 }
+hello
